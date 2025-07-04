5605b4fc93ad9286ba60f0d59c3e67bfe82be418
simon maina <simonkamu924@gmail.com>
2025-07-05 02:36:44 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def home_page():
+    
+    return render_template("home.html")
+
+@app.route("/projects")
+def projects_page():
+    
+    return render_template("projects.html")
+@app.route("/contact")
+def contact_page():
+    
+    return render_template("contact.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
